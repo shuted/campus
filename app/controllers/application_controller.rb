32bd49c8a76c5e8fc401cf5af89bc8015d19d048
shuted <shuted@rpi.edu>
2016-04-14 15:37:20 +0000

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello 
-    render text: "44444 444 4444 44"
-  end
+  
 end
