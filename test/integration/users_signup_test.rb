2596e312a1da0251ce6a41de1ebc1f71639eba19
shuted <shuted@rpi.edu>
2016-04-18 19:45:37 +0000

    Finish user sign up

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup info" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: {name: "",
+                              email: "user@invalid",
+                              password: "444",
+                              password_confirmation: "4444"}
+    end
+    assert_template 'users/new'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+    assert_template 'users/show'
+  end
+end
