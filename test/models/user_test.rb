0bc10d87fb2213c623622e549ad15e6ca7b1a31d
shuted <shuted@rpi.edu>
2016-04-26 22:52:05 +0000

    added microposts for users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -67,4 +67,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')
   end
   
+  test "microposts should be destroyed too" do 
+    @user.save
+    @user.microposts.create!(content: "444 444")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+  
 end
