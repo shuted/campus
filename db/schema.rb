fe3ce471323dc16c715f63a03f0923fd8255bfca
shuted <shuted@rpi.edu>
2016-05-06 02:28:29 +0000

    started to add location getting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505204843) do
+ActiveRecord::Schema.define(version: 20160505214202) do
 
   create_table "ahoy_events", force: :cascade do |t|
     t.integer  "visit_id"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20160505204843) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
